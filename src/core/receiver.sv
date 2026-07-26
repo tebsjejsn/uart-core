038e3b2d02a1bffa8abc412746a048409855748e
Tej Patel <tej.patel025@gmail.com>
2026-07-25 21:42:22 -0400

    feat(receiver.sv): add module to gather external inputs

---
+++ src/core/receiver.sv
@@ -0,0 +1,85 @@
+module receiver(
+    input  logic        clk,
+    input  logic        reset,
+    input  logic        tick,
+    input  logic        rx,
+    output logic [31:0] data,
+    output logic        en
+);
+    typedef enum logic [1:0] {
+        IDLE,
+        START,
+        SAMPLE,
+        FINISH
+    } statetype;
+
+    statetype state;
+    statetype nextState;
+
+    logic       rxSync1;
+    logic       rxSync2;
+    logic [5:0] initialCount;
+    logic [3:0] bitCount;
+
+    always_ff @(posedge clk) begin
+        rxSync1 <= rx;
+        rxSync2 <= rxSync1;
+    end
+
+    always_ff @(posedge clk, posedge reset) begin
+        if (reset) begin
+            state <= IDLE;
+            initialCount <= '0;
+            bitCount <= '0;
+        end
+        else begin
+            if (state == IDLE) begin
+                initialCount <= '0;
+                bitCount <= '0;
+            end
+            else if (state == START) begin
+                if (tick == '1) 
+                    if (initialCount == 6'd7)
+                        initialCount <= '0;
+                    else
+                        initialCount <= initialCount + 1;
+            end
+            else if (state == SAMPLE) begin
+                if (tick == '1) begin
+                    if (initialCount == 6'd15)
+                        if (bitCount == 4'd7) begin
+                            initialCount <= '0;
+                            bitCount <= '0;
+                        end
+                        else begin
+                            bitCount <= bitCount + 1;
+                            initialCount <= '0;
+                        end
+                    else
+                        initialCount <= initialCount + 1;
+                end
+            end
+
+            state <= nextState;
+        end
+    end
+
+
+    always_comb begin
+        nextState = state;
+
+        case (state)
+            IDLE:
+                if (rxSync2 == '0)
+                    nextState = START;
+            START:
+                if (tick == '1 && initialCount == 6'd7)
+                    nextState = SAMPLE;
+            SAMPLE:
+                if (tick == '1 && initialCount == 6'd15)
+                    if (bitCount == 4'd7)
+                        nextState = FINISH;
+            default: nextState = IDLE;
+        endcase
+    end
+endmodule
