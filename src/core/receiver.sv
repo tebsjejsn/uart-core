77a829dcf41d4df4a1189c54e22d7dde2f4a4dbb
Tej Patel <tej.patel025@gmail.com>
2026-07-26 13:38:33 -0400

    fix(receiver.sv): add final state in receiver

--- src/core/receiver.sv
+++ src/core/receiver.sv
@@ -3,8 +3,9 @@ module receiver(
     input  logic        reset,
     input  logic        tick,
     input  logic        rx,
-    output logic [31:0] data,
-    output logic        en
+    output logic [7:0] data,
+    output logic        en,
+    output logic        frameError
 );
     typedef enum logic [1:0] {
         IDLE,
@@ -18,6 +19,7 @@ module receiver(
 
     logic       rxSync1;
     logic       rxSync2;
+    logic [7:0] word;
     logic [5:0] initialCount;
     logic [3:0] bitCount;
 
@@ -31,11 +33,13 @@ module receiver(
             state <= IDLE;
             initialCount <= '0;
             bitCount <= '0;
+            word <= '0;
         end
         else begin
             if (state == IDLE) begin
                 initialCount <= '0;
                 bitCount <= '0;
+                word <= '0;
             end
             else if (state == START) begin
                 if (tick == '1) 
@@ -46,7 +50,10 @@ module receiver(
             end
             else if (state == SAMPLE) begin
                 if (tick == '1) begin
-                    if (initialCount == 6'd15)
+                    if (initialCount == 6'd15) begin
+                        word <= word >> 1;
+                        word[7] <= rxSync2;
+
                         if (bitCount == 4'd7) begin
                             initialCount <= '0;
                             bitCount <= '0;
@@ -55,18 +62,29 @@ module receiver(
                             bitCount <= bitCount + 1;
                             initialCount <= '0;
                         end
+                    end
                     else
                         initialCount <= initialCount + 1;
                 end
             end
-
+            else if (state == FINISH) begin
+                if (tick == '1) begin
+                    if (initialCount != 6'd15) begin
+                        initialCount <= initialCount + 1;
+                    end
+                    else
+                        initialCount <= '0;
+                end
+            end
             state <= nextState;
         end
     end
 
-
     always_comb begin
         nextState = state;
+        data = '0;
+        en = '0;
+        frameError = '0;
 
         case (state)
             IDLE:
@@ -79,6 +97,19 @@ module receiver(
                 if (tick == '1 && initialCount == 6'd15)
                     if (bitCount == 4'd7)
                         nextState = FINISH;
+            FINISH: 
+                if (tick == '1 && initialCount == 6'd15)
+                    if (rxSync2 == '1) begin
+                        nextState = IDLE;
+
+                        data = word;
+                        en = '1;
+                        frameError = '0;
+                    end
+                    else begin
+                        nextState = IDLE;
+                        frameError = '1; 
+                    end
             default: nextState = IDLE;
         endcase
     end
